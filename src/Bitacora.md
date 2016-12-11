24fae60860dd28d8907b8993d40f123907f91c57
Alejandro Hernandez <usercall.hm@gmail.com>
2016-12-11 13:12:33 -0500

    Create api

---
+++ src/Bitacora.md
@@ -0,0 +1,8 @@
+#crear api
+[Mira el Video](https://www.youtube.com/watch?v=lxvLxgmdSRc)
+
+#Cadena creacion
+
+Recomendacion utilizar interprete bash
+
+    bee api juegoApiV1 -driver=postgres -conn=postgres://admin:admin@127.0.0.1/admin?sslmode=disable
---
+++ src/juegoApiV1/routers/router.go
@@ -0,0 +1,38 @@
+// @APIVersion 1.0.0
+// @Title beego Test API
+// @Description beego has a very cool tools to autogenerate documents for your API
+// @Contact astaxie@gmail.com
+// @TermsOfServiceUrl http://beego.me/
+// @License Apache 2.0
+// @LicenseUrl http://www.apache.org/licenses/LICENSE-2.0.html
+package routers
+
+import (
+	"juegoApiV1/controllers"
+
+	"github.com/astaxie/beego"
+)
+
+func init() {
+	ns := beego.NewNamespace("/v1",
+
+		beego.NSNamespace("/juego",
+			beego.NSInclude(
+				&controllers.JuegoController{},
+			),
+		),
+
+		beego.NSNamespace("/jugador",
+			beego.NSInclude(
+				&controllers.JugadorController{},
+			),
+		),
+
+		beego.NSNamespace("/puntaje",
+			beego.NSInclude(
+				&controllers.PuntajeController{},
+			),
+		),
+	)
+	beego.AddNamespace(ns)
+}
